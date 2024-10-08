1867cd44679beac8ced940ab184082843b74b5ea
Семён Першин <sempershin@mail.ru>
2024-10-08 13:20:03 +0300

    Выполнено ДЗ (Обработка событий)

--- src/js/GoblinGame.js
+++ src/js/GoblinGame.js
@@ -1,25 +1,53 @@
 export class GoblinGame {
     constructor() {
         this.goblin = document.getElementById("goblin");
+        this.cells = document.querySelectorAll(".game_field > div");
+
         this.blinkInterval = 1000;
         this.gameStart = undefined;
-        this.cells = document.querySelectorAll(".game_field > div");
         this.currentPosition = 0;
+
+        this.scoreDiv = document.querySelector(".score");
+        this.missesDiv = document.querySelector(".misses");
+
+        this.loseScreen = document.querySelector(".lose_screen")
     }
 
     startGame() {
+        this.scoreDiv.textContent = 0;
+        this.missesDiv.textContent = 0;
+
         this.gameStart = setInterval(() => {
-            let shift = Math.ceil(Math.random() * 15);
-            if (this.currentPosition + shift > 15) {
-                this.currentPosition = this.currentPosition + shift - 15
+            let shift = Math.ceil(Math.random() * 14);
+            if (this.currentPosition + shift > 14) {
+                this.currentPosition = this.currentPosition + shift - 14
             } else {
                 this.currentPosition = this.currentPosition + shift
             }
             this.cells[this.currentPosition].appendChild(this.goblin)
+
+            if(this.goblin.classList.contains("hide")) {
+                this.goblin.classList.remove("hide");
+            } else {
+                this.missesDiv.textContent++;
+            }
+
+            if (this.missesDiv.textContent >= 5) {
+                this.endGame()
+            }
         }, this.blinkInterval)
     }
 
     endGame() {
-        clearInterval(this.gameStart)
+        clearInterval(this.gameStart);
+        this.loseScreen.classList.remove("hide");
+        this.loseScreen.textContent = `Жаль, но вы проиграли. Ваш счет: ${this.scoreDiv.textContent}`
     }
+
+    killGoblin() {
+        this.goblin.classList.add("hide");
+        this.scoreDiv.textContent++;
+    }
+
+
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,8 @@
 import {GoblinGame} from "./GoblinGame.js"
 
-let isStart = true;
-
 const goblin = new GoblinGame();
 goblin.startGame();
 
-addEventListener("click", () => {
-    if(isStart) {
-        goblin.endGame();
-        isStart = !isStart;
-    } else {
-        goblin.startGame();
-        isStart = !isStart;
-    }
+goblin.goblin.addEventListener('click', () => {
+    goblin.killGoblin()
 })
